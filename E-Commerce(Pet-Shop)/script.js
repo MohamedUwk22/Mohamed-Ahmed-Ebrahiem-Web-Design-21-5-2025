40791cc760502545b64d8da7c6a6fc6cfac1439f
MohamedUwk22 <mohamed.uwk2200@gmail.com>
2025-06-25 16:18:17 +0300

    Version-1

---
+++ E-Commerce(Pet-Shop)/script.js
@@ -0,0 +1,6 @@
+// const myModal = document.getElementById("myModal");
+// const videoIcon = document.getElementById("videoIcon");
+// videoIcon.addEventListener("click", () => {
+//   myModal.classList.add("modal-active");
+//   myModal.classList.remove("modal");
+// });
